740a737b5c78299077f3db5a294be975b0bab182
mihret1 <90724655+mihret1@users.noreply.github.com>
2024-02-29 11:26:58 +0300

    function to handle scroll change

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,30 @@
+import { useState,useEffect } from 'react'
 
 function Navbar() {
-  return (
-    <div>
-    <div className="h-7 bg-green-800">hello</div>
-    <div className=" bg-slate-400 h-[115px] ">hello</div>
-    <div className="h-14 bg-slate-300"></div>
+    const [isTransparet,setIsTransparent]=useState(false)
 
-    </div>
+    useEffect(()=>{
+        const handleTrasparency=()=>{
+            if(window.scrollY >=90){
+                setIsTransparent(true)
+            }else{
+                setIsTransparent(false)
+            }
+        }
+
+        window.addEventListener('scroll',handleTrasparency)
+        return()=>{window.removeEventListener('scroll',handleTrasparency)}
+    },[])
+  return (
+    <>
+  {  <div>
+        <div className="h-7 bg-green-800">hello</div>
+        <div className=" bg-slate-400 h-[115px] ">hello</div>
+        <div className="h-14 bg-slate-300"></div>
+    </div>}
+
+    <div className='h-[120px] bg-red-500'></div>
+    </>
   )
 }
 
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,14 @@
 
+import img5 from '../../assets/h5.jpg'
+
 function Home() {
+
+
   return (
-    <div>hello am Home</div>
-  )
+    <div className='h-[700px] bg-cover bg-no-repeat' style={{ backgroundImage:`url(${img5})` }}>
+    
+   </div>
+    )
 }
 
 export default Home
