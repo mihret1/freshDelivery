7ea6532c28a859362ea060deb161543a9f145d6f
mihret1 <90724655+mihret1@users.noreply.github.com>
2024-03-04 11:02:17 +0300

    done the navlink for transparent part

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -55,26 +55,88 @@ function Navbar() {
                 <h1 className='text-green-700 font-bold text-xl'>(+251)934567892 </h1>
             </div>
           </div>
-         <div className="h-12">
-           <div className="custom-select p-5">
-                <select>
-                    <option value="0">Select car:</option>
-                    <option value="1">Audi</option>
-                    <option value="2">BMW</option>
-                    <option value="3">Citroen</option>
-                    <option value="4">Ford</option>
-                    <option value="5">Honda</option>
-                    <option value="6">Jaguar</option>
-                    <option value="7">Land Rover</option>
-                    <option value="8">Mercedes</option>
-                    <option value="9">Mini</option>
-                    <option value="10">Nissan</option>
-                    <option value="11">Toyota</option>
-                    <option value="12">Volvo</option>
+         <div className="h-16">
+           
+           <div className="  flex flex-row  gap-3 justify-center pt-[1%] text-lg font-semibold">
+
+                <select className='border-none outline-none'>
+                    <option value="0"><a href='/'>Type</a></option>
+                    <option value="0"><a href='/'>Hotel</a></option>
+                    <option value="0"><a href='/'>park</a></option>
+                    <option value="0"><a href='/'>muesuem</a></option>
+                    <option value="0"><a href='/'>churches</a></option>
+                    <option value="0"><a href='/'>waterBody</a></option>
+                    <option value="0"><a href='/'>mountain</a></option>
+                    <option value="0"><a href='/'>crafts</a></option>
+
+                 
                 </select>
+
+                <select className='border-none outline-none'>
+                    <option value="0"><a href='/'>Country</a></option>
+                    <option value="0"><a href='/'>Addis Abeba</a></option>
+                    <option value="0"><a href='/'>arbaminch</a></option>
+                    <option value="0"><a href='/'>hawwasa</a></option>
+                    <option value="0"><a href='/'>bahirdar</a></option>
+                    <option value="0"><a href='/'>gonder</a></option>
+                    <option value="0"><a href='/'>axsum</a></option>
+                    <option value="0"><a href='/'>zway</a></option>
+                    <option value="0"><a href='/'>butajira</a></option>
+                    <option value="0"><a href='/'>afar</a></option>
+                    <option value="0"><a href='/'>somalia</a></option>
+                    <option value="0"><a href='/'>tigria</a></option>
+                    <option value="0"><a href='/'>oromia</a></option>
+                    <option value="0"><a href='/'>gambella</a></option>
+                    <option value="0"><a href='/'>hossana</a></option>
+
+
+                 
+                </select>
+
+                <select className='border-none outline-none'>
+                    <option value="0"><a href='/'>Packages</a></option>
+                    <option value="0"><a href='/'>Group</a></option>
+                    <option value="0"><a href='/'>indiviual</a></option>   
+                </select>
+
+                <select className='border-none outline-none'>
+                    <option value="0"><a href='/'>Pages</a></option>
+                    <option value="0"><a href='/'>Signup</a></option>
+                    <option value="0"><a href='/'>Login</a></option>
+                    <option value="0"><a href='/'>blog</a></option>
+                    <option value="0"><a href='/'>services</a></option>
+                    <option value="0"><a href='/'>contact</a></option>
+                    <option value="0"><a href='/'>about-us</a></option>
+                    <option value="0"><a href='/'>packages</a></option>
+                    <option value="0"><a href='/'>Country</a></option>
+                    <option value="0"><a href='/'>type</a></option>
+
+
+                 
+                </select>
+
+               <button className='px-3'><a href='/'>Services</a></button>
+
+               <button className='px-3'><a href='/'>About-Us</a></button>
+
+               <button className='px-3'><a href='/'>Blog</a></button>
+
+               <button className='px-3'><a href='/'>Contact</a></button>
+               
+               <button className='bg-[#2fac82] px-8 py-1'><a href='/'>Login</a></button>
+
+
+              
           </div>
+
+
          </div>
-      </div> 
+
+
+    </div> 
+
+
+
      
     :
     <div className='fixed w-[100%]'>
